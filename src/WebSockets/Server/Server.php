55fc7e3e4d41be7b0a304704841b2d7b7197ac0a
Travis Spears <travisspears0@gmail.com>
2015-06-04 23:01:14 +0200

    sockets working on telnet

--- src/WebSockets/Server/Server.php
+++ src/WebSockets/Server/Server.php
@@ -106,10 +106,6 @@ class Server {
 		    	}
 		    }
 		}
-		/*
-		socket_close($client);
-		socket_close($socket);
-		*/
 	}
 
 	static function write($message,$author="SERVER") {
@@ -121,34 +117,3 @@ class Server {
 	}
 
 }
-
-/*
-<?php
-// set ip and port
-$host = "127.0.0.1";
-$port = 4096;
-// don't timeout!
-set_time_limit(0);
-// create socket
-$socket = socket_create(AF_INET, SOCK_STREAM, 0);
-// bind socket to port
-$result = socket_bind($socket, $host, $port);
-// start listening for connections
-$result = socket_listen($socket, 3);
-
-// accept incoming connections
-// spawn another socket to handle communication
-$spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
-// read client input
-$input = socket_read($spawn, 1024) or die("Could not read input\n");
-// clean up input string
-$input = trim($input);
-echo "Client Message : ".$input."<br />";
-// reverse client input and send back
-$output = strrev($input) ."<br />";
-socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
-// close sockets
-socket_close($spawn);
-socket_close($socket);
-?>
-*/
